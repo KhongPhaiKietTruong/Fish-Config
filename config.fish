5855fd0a6b9d62124f7e68fbb7def6d2eb7b3794
kiettruong <truonganhkiet291006@gmail.com>
2026-06-27 23:26:21 +0700

    first commit

---
+++ config.fish
@@ -0,0 +1,52 @@
+if status is-interactive
+    # No greeting
+    set fish_greeting
+    #
+    # Use starship
+    function starship_transient_prompt_func
+        starship module character
+    end
+    if test "$TERM" != linux
+        starship init fish | source
+        enable_transience
+    end
+
+    # Aliases
+    # kitty doesn't clear properly so we need to do this weird printing
+    #alias clear "printf '\033[2J\033[3J\033[1;1H'"
+    if test "$TERM" != linux
+        alias ls 'eza --icons'
+    end
+end
+
+# Khai báo các file sẽ bị bỏ qua khi tìm kiếm vào một biến để tái sử dụng 
+set exclude --hidden --exclude .git --exclude node_modules --exclude .cache --exclude __pycache__
+
+# Đặt alias 
+alias cdi="nvim (fd --type d | fzf)"
+
+# Dùng zoxide 
+zoxide init fish | source
+
+# Dùng fzf
+fzf --fish | source
+
+# Đặt Neovim làm Editor mặc định trong Fish Shell (mở file bằng nvim trong yazi) 
+set -gx EDITOR nvim
+
+# Sửa file nhanh 
+function vii
+    set -l file (fd --type f $exclude | fzf)
+    if test -n "$file"
+        nvim $file
+    end
+end
+
+# Vào thư mục nhanh 
+function cdi
+    set -l dir (fd --type d --hidden | fzf)
+
+    if test -n "$dir"
+        nvim $dir
+    end
+end
